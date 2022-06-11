d8238229b7c04fdad6467b1a145743373acfad8b
FidanMemmedova <5TAWDL9@code.edu.az>
2022-06-11 17:14:12 +0400

    17:14

---
+++ Juan/Areas/AdminPanel/Controllers/SlideController.cs
@@ -0,0 +1,32 @@
+﻿using Juan.DAL;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Juan.Areas.AdminPanel.Controllers
+{
+    public class SlideController : Controller
+    { 
+        [Area("AdminPanel")]
+        private AppDbContext _context { get; }
+        private IWebHostEnvironment _env { get; }
+        public SlideController(AppDbContext context, IWebHostEnvironment env)
+        {
+            _context = context;
+            _env = env;
+        }
+        public IActionResult Index()
+        {
+            return View(_context.Slides);
+        } 
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        
+    }
+}
