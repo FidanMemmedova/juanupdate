4a331ab7cba5aacd26f8140bb16908fe01fc3942
FidanMemmedova <5TAWDL9@code.edu.az>
2022-06-11 18:20:02 +0400

    18:19 start

--- Juan/Areas/AdminPanel/Controllers/SlideController.cs
+++ Juan/Areas/AdminPanel/Controllers/SlideController.cs
@@ -1,17 +1,19 @@
 ﻿using Juan.DAL;
+using Juan.Models;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
-using Juan.Models;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace Juan.Areas.AdminPanel.Controllers
+
+namespace WEB.Areas.AdminPanel.Controllers
 {
     [Area("AdminPanel")]
     public class SlideController : Controller
-    { 
+    {
         private AppDbContext _context { get; }
         private IWebHostEnvironment _env { get; }
         public SlideController(AppDbContext context, IWebHostEnvironment env)
@@ -22,12 +24,58 @@ namespace Juan.Areas.AdminPanel.Controllers
         public IActionResult Index()
         {
             return View(_context.Slides);
-        } 
+        }
         public IActionResult Create()
         {
             return View();
         }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(Slide slide)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View();
+            }
+            if (!slide.Photo.CheckFileSize(200))
+            {
+                ModelState.AddModelError("Photo", "Max size image must be less than 200kb");
+                return View();
+            }
+            if (!slide.Photo.CheckFileType("image/"))
+            {
+                ModelState.AddModelError("Photo", "Type of file must be image");
+                return View();
+            }
+            //return Json(slide.Photo.ContentType.Contains("image/"));
+            //return Json("Ok");
 
-        
+            slide.Url = await slide.Photo.SaveFileAsync(_env.WebRootPath, "img");
+            await _context.Slides.AddAsync(slide);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        {
+            if (id == null)
+            {
+                return BadRequest();
+            }
+            var slider = _context.Slides.Find(id);
+            if (slider == null)
+            {
+                return NotFound();
+            }
+            var path = Helper.GetPath(_env.WebRootPath, "img", slider.Url);
+            if (System.IO.File.Exists(path))
+            {
+                System.IO.File.Delete(path);
+            }
+            _context.Slides.Remove(slider);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+     
+       
     }
 }
