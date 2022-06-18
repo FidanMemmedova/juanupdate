9392772579e1f7e25f405b1802e2d73eeccb2df2
FidanMemmedova <5TAWDL9@code.edu.az>
2022-06-18 18:13:35 +0400

    /

---
+++ Juan/Migrations/20220618140422_createCardTable.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Juan.Migrations
+{
+    public partial class createCardTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Percent",
+                table: "Discounts",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Percent",
+                table: "Discounts");
+        }
+    }
+}
---
+++ Juan/Models/ShopProduct.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Juan.Models
+{
+    public class ShopProduct
+    {
+        public int Id { get; set; }
+        public string Image { get; set; }
+        public string ProductName { get; set; }
+        public int Price { get; set; }
+        public int Discount { get; set; }
+    }
+}
